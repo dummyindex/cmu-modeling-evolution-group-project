68a920500e807984b5f3bf5f2d64811fad4af4ff
dummyindex <dummtindex@gmail.com>
2025-05-02 01:57:08 -0400

    add sh script for runnig the graph-based simluations

---
+++ run_trials.sh
@@ -0,0 +1,13 @@
+python ./graph_sim_v2.py --graph_N=100 --generations=10
+python ./graph_sim_v2.py --graph_N=100 --generations=100
+python ./graph_sim_v2.py --graph_N=100 --generations=1000
+
+python ./graph_sim_v2.py --graph_N=500 --generations=10
+python ./graph_sim_v2.py --graph_N=500 --generations=100
+python ./graph_sim_v2.py --graph_N=500 --generations=1000
+
+
+python ./graph_sim_v2.py --graph_N=1000 --generations=10
+python ./graph_sim_v2.py --graph_N=1000 --generations=100
+python ./graph_sim_v2.py --graph_N=1000 --generations=1000
+
